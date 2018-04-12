486c7b80e65cfdea7c6d3d65c5eefc2ac03fa94b
NAVDEEP SIDHU <sidhu44@gmail.com>
2018-04-12 06:59:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
  if ( string.toLowerCase() === string)
    return "I can't hear you!"
  else if( string.toUpperCase() === string)
    return "YES INDEED!"
  else
    return "I love you, too."
+=======
+  return string.toUpperCase()
+>>>>>>> c814d10ced83dc7091d7dcf666cea9da82479b45
 }
 
